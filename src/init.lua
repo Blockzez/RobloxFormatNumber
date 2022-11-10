fe8df6b08bc309c07f4c9ce68b5c115732e3faf6
Nicholas Foreman <nicholas.w.foreman@outlook.com>
2022-11-09 23:06:07 -0600

    Create init file for the project

---
+++ src/init.lua
@@ -0,0 +1,10 @@
+local FormatNumber = {}
+
+FormatNumber.Version = require(script.Version)
+
+FormatNumber.Main = require(script.Main)
+FormatNumber.Simple = require(script.Simple)
+
+FormatNumber.Test = require(script.Test)
+
+return FormatNumber
